3b2f1fea5d3d554155e60206a1b46ae818737bd0
Erfan Moghadasi <erfan.moqadasii@gmail.com>
2022-05-04 19:22:57 +0430

    create Services component

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,11 +1,107 @@
-import React from 'react';
+import React from "react";
+import { BiCheck } from "react-icons/bi";
+import './services.css'
 
 const Services = () => {
-    return (
-        <section id='services'>
-            Services
-        </section>
-    );
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        {/* ============ UI/UX DESIGN ========== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* ============ WEB DEVELOPMENT ========== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* ============ CONTENT CREATION ========== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
