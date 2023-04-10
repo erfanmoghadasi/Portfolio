5904073434566186e5ea90eb68b6159c57e29cbd
erfan moghadasi <erfan.moqadasii@gmail.com>
2023-04-10 16:26:30 +0330

    navbar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -11,13 +11,13 @@ import {BiMessageSquareDetail} from 'react-icons/bi'
 const Nav = () => {
     const [activeNav , setActiveNav] = useState('#')
     return (
-        <nav>
-            <a href='/#' className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><AiOutlineHome /></a>
-            <a href='/#about' className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
-            <a href='/#experience' className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBook /></a>
-            <a href='/#services' className={activeNav === '#services' ? 'active' : ''} onClick={() => setActiveNav('#services')}><RiServiceLine /></a>
-            <a href='/#contact' className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
-        </nav>
+        <ul id='nav-list'>
+            <li  className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><AiOutlineHome /></li>
+            <li  className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></li>
+            <li  className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBook /></li>
+            <li  className={activeNav === '#services' ? 'active' : ''} onClick={() => setActiveNav('#services')}><RiServiceLine /></li>
+            <li  className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></li>
+        </ul>
     );
 };
 
